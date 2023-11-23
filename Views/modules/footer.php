167449b819f16924366524d1cc05dc01c8133aa7
julito <jcponce87@gmail.com>
2023-11-23 14:39:45 -0600

    footer

--- Views/modules/footer.php
+++ Views/modules/footer.php
@@ -20,7 +20,6 @@
                 <li><a href="about.html" class="py-2 d-block">About</a></li>
                 <li><a href="services.html" class="py-2 d-block">Services</a></li>
                 <li><a href="#" class="py-2 d-block">Term and Conditions</a></li>
-                <li><a href="#" class="py-2 d-block">Best Price Guarantee</a></li>
                 <li><a href="#" class="py-2 d-block">Privacy &amp; Cookies Policy</a></li>
               </ul>
             </div>
@@ -29,10 +28,7 @@
              <div class="ftco-footer-widget mb-4">
               <h2 class="ftco-heading-2">Customer Support</h2>
               <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">FAQ</a></li>
-                <li><a href="#" class="py-2 d-block">Payment Option</a></li>
-                <li><a href="#" class="py-2 d-block">Booking Tips</a></li>
-                <li><a href="#" class="py-2 d-block">How it works</a></li>
+              
                 <li><a href="#" class="py-2 d-block">Contact Us</a></li>
               </ul>
             </div>
@@ -42,9 +38,9 @@
             	<h2 class="ftco-heading-2">Have a Questions?</h2>
             	<div class="block-23 mb-3">
 	              <ul>
-	                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+	                <li><span class="icon icon-map-marker"></span><span class="text">Biscayne Blvd Miami, 33160 FL, USA</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+305.204.6887</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@travelvipmiami.com</span></a></li>
 	              </ul>
 	            </div>
             </div>
@@ -54,7 +50,7 @@
           <div class="col-md-12 text-center">
 
             <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Travel VIP Miami 
   <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
           </div>
         </div>
